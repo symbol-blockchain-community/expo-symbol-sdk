adc6ad21da68f615af415dec0e6ddeac917ed1e6
matsukawatoshiya <matsukawa5955@gmail.com>
2024-03-02 07:52:24 +0200

    add tryDecodeDeprecated

--- src/service/message.ts
+++ src/service/message.ts
@@ -38,19 +38,19 @@ export class MessageEncoder {
 
   /**
    * Tries to decode encoded message.
-   * @param {Uint8Array} recipientPublicKey Recipient's public key.
+   * @param {Uint8Array} senderPublicKey Recipient's public key.
    * @param {Uint8Array} encodedMessage Encoded message.
    * @returns {TryDecodeResult} Tuple containing decoded status and message.
    */
-  tryDecode(recipientPublicKey: Uint8Array | string, encodedMessage: Uint8Array): TryDecodeResult {
-    recipientPublicKey =
-      typeof recipientPublicKey === "string"
-        ? Convert.hexToUint8(recipientPublicKey)
-        : recipientPublicKey;
+  tryDecode(senderPublicKey: Uint8Array | string, encodedMessage: Uint8Array): TryDecodeResult {
+    senderPublicKey =
+      typeof senderPublicKey === "string"
+        ? Convert.hexToUint8(senderPublicKey)
+        : senderPublicKey;
     if (1 === encodedMessage[0]) {
       const [result, message] = filterExceptions(
         () =>
-          decodeAesGcm(deriveSharedKey, this._privateKey, recipientPublicKey as Uint8Array, encodedMessage.subarray(1)),
+          decodeAesGcm(deriveSharedKey, this._privateKey, senderPublicKey as Uint8Array, encodedMessage.subarray(1)),
         ["Unsupported state or unable to authenticate data", "invalid point"]
       );
       if (result) return { isDecoded: true, message: utf8ArrayToString(message as Uint8Array) };
@@ -75,4 +75,26 @@ export class MessageEncoder {
     );
     return Convert.hexToUint8('01' + tag + initializationVector + cipherText);
   }
+
+  /**
+	 * Tries to decode encoded message.
+	 * @deprecated This function is only provided for compatability with the original Symbol wallets.
+	 *             Please use `tryDecode` in any new code.
+	 * @param {PublicKey} senderPublicKey Recipient's public key.
+	 * @param {Uint8Array} encodedMessage Encoded message
+	 * @returns {TryDecodeResult} Tuple containing decoded status and message.
+	 */
+	tryDecodeDeprecated(senderPublicKey: Uint8Array | string, encodedMessage: Uint8Array): TryDecodeResult {
+    senderPublicKey =
+      typeof senderPublicKey === "string"
+        ? Convert.hexToUint8(senderPublicKey)
+        : senderPublicKey;
+		const encodedHexString = Convert.uint8ToHex(encodedMessage.subarray(1));
+		if (1 === encodedMessage[0] && Convert.isHexString(encodedHexString)) {
+			// wallet additionally hex encodes
+			return this.tryDecode(senderPublicKey, new Uint8Array([1, ...Convert.hexToUint8(encodedHexString)]));
+		}
+
+		return this.tryDecode(senderPublicKey, encodedMessage);
+	}
 }
